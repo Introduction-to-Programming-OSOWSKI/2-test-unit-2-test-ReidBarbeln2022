92aff97ac2e5dc0b3d750b555a6639734b12d313
ReidBarbeln2022 <814684@mystma.org>
2021-01-15 16:15:20 +0000

    unit 2 test try 1

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(f):
+    if f <= 32:
+        return "solid"
+    elif 32 < f < 211 :
+        return "liqid"
+    else  :
+        return "gas"
+print (waterState(300))
+
+def isDozen(d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+print (isDozen(35))
+
+def toGerman( y ):
+    if y == "yes":
+        return "ja"
+    else:
+        return "nein"
+print (toGerman("no"))
+
+def stopLight(c):
+    if c == "green":
+        return "go"
+    elif c== "yellow":
+        return "slow"
+    else:
+        return "stop"
+print (stopLight("green"))
